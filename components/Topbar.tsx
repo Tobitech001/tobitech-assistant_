3788306225fdc42fa45e3bdd010f02e3519c4a58
Tobitech001 <moshoodabdulmujibtobiloba@gmail.com>
2025-07-29 02:21:57 +0100

    Update Topbar.tsx

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,12 +1,4 @@
-import { Bell } from "lucide-react";
-
+// components/Topbar.tsx
 export default function Topbar() {
-  return (
-    <header className="flex justify-between items-center bg-gray-800 p-4 rounded-xl text-white shadow">
-      <h2 className="text-lg font-semibold">Welcome, Tobiloba 👋</h2>
-      <div className="flex items-center space-x-4">
-        <Bell className="w-5 h-5" />
-      </div>
-    </header>
-  );
+  return <div>Topbar</div>;
 }
